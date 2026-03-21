b9348a102d7900a13f3f39a32892961347ac7f9f
Vivien Henz <vhenz@college.harvard.edu>
2026-03-21 13:01:05 -0400

    cuda err

--- StyleTTS2/Modules/discriminators.py
+++ StyleTTS2/Modules/discriminators.py
@@ -50,7 +50,7 @@ class SpecDiscriminator(nn.Module):
 
         fmap = []
         y = y.squeeze(1)
-        y = stft(y, self.fft_size, self.shift_size, self.win_length, self.window.to(y.get_device()))
+        y = stft(y, self.fft_size, self.shift_size, self.win_length, self.window.to(y.device))
         y = y.unsqueeze(1)
         for i, d in enumerate(self.discriminators):
             y = d(y)
